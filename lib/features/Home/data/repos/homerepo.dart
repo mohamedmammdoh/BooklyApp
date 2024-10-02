5662edb33007abbe444561575763c0734a1023c8
mohamedmammdoh <mohamedmammdohharby@gmail.com>
2024-10-02 20:33:10 +0300

    create home repo and error folder and books model

---
+++ lib/features/Home/data/repos/homerepo.dart
@@ -0,0 +1,8 @@
+import 'package:booklyapp/core/Errors/failers.dart';
+import 'package:booklyapp/features/Home/data/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class HomeRepo {
+  Future<Either<Failer, List<BookModel>>> fetchBestSellerBooks();
+  Future<Either<Failer, List<BookModel>>> fetchFeaturedBooks();
+}
